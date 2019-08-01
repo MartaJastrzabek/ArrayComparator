c31ec6db0aa41417f2c82129e7fb4edfb4c3fa41
Marta <marta.jastrzabek@gmail.com>
2019-08-01 11:14:03 +0200

    Upgraded version, now program is able to compare 2 dimensions arrays also.

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,20 +1,42 @@
 public class ArrayComparator {
     public static boolean compare(int[] array, int[] array2){
-        if(array.length==0 || array2.length == 0){
-            System.out.print("Błąd pusta tablica, ");
+        if(check(array,array2)){
+            for (int i = 0; i<array.length; i++){
+                if(array[i]!=array2[i]){
+                    return false;
+                }
+            }
+        } else {
             return false;
         }
+        return true;
+    }
 
-        if(array.length!=array2.length){
-            return false;
-        }
-
-        for (int i = 0; i<array.length; i++){
-            if(array[i]!=array2[i]){
+    public static boolean compare(int[][] array, int[][] array2){
+        for(int i = 0; i<array.length; i++){
+            if (check(array[i], array2[i])) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (array[i][j] != array2[i][j]) {
+                        return false;
+                    }
+                }
+            }else {
                 return false;
             }
         }
-
         return true;
     }
+
+    private static boolean check(int[] array, int[] array2){
+        if(array.length==0 || array2.length == 0){
+            System.out.print("Błąd pusta tablica, ");
+            return false;
+        }else if (array.length!=array2.length){
+            System.out.print("Błąd tablice mają różny rozmiar, ");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -5,11 +5,36 @@ public class ArrayTest {
         int[] tab3 = {1,2,3,4,5,6,7};
         int[] tab4 = {7,8,9,4,5,6,1,2,3};
         int[] tab5 = {};
+        int[][] tab6 = {
+                {1,2,3},
+                {3,4,5}
+        };
+        int[][] tab7 = {
+                {1,2,3},
+                {3,4,5}
+        };
+
+        int[][] tab8 = {
+                {4,5,6},
+                {6,5,4}
+        };
+
+        int[][] tab9 = {
+                {4,5,6},
+                {6,5}
+        };
+
+
+
 
         System.out.println(ArrayComparator.compare(tab,tab2));
         System.out.println(ArrayComparator.compare(tab,tab3));
         System.out.println(ArrayComparator.compare(tab,tab4));
         System.out.println(ArrayComparator.compare(tab,tab5));
+        System.out.println(ArrayComparator.compare(tab8,tab6));
+        System.out.println(ArrayComparator.compare(tab6,tab7));
+        System.out.println(ArrayComparator.compare(tab8,tab9));
+
 
     }
 }
